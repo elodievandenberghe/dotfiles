4a279d3e7049f22d84c320a2d6d6798a52390408
Elodie Vandenberghe <elodie.vandenberghe@outlook.com>
2026-08-29 22:18:54 +0200

    sjfsa;fa

--- nixos/modules/display/de_wm/sway.nix
+++ nixos/modules/display/de_wm/sway.nix
@@ -6,33 +6,32 @@
 }:
 
 {
+  options.sway.enable = lib.mkEnableOption "Enable Sway";
 
-  options = {
-    sway.enable = lib.mkEnableOption "Enables qtile ";
+  config = lib.mkIf config.sway.enable {
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      mako
+    ];
+
+    # Enables Gnome Keyring to store secrets for applications.
+    services.gnome.gnome-keyring.enable = true;
+
+    # Enable Sway.
+    programs.sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+    };
+
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+          user = "greeter";
+        };
+      };
+    };
   };
-
-  config = lib.mkIf config.qtile.enable {
-   environment.systemPackages = with pkgs; [
-   	wl-clipboard # Copy/Paste functionality.
-        mako # Notification utility.
-  ];
-
-  # Enables Gnome Keyring to store secrets for applications. 
-  services.gnome.gnome-keyring.enable = true;
-
-  # Enable Sway.
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-  };
-  services.greetd = {                                                      
-  enable = true;                                                         
-  settings = {                                                           
-    default_session = {                                                  
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
-      user = "greeter";                                                  
-    };                                                                   
-  };                                                                     
-};
 }
+
